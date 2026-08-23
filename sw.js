501263405c65a4a7590147cc6b4e024cc8c0dab8
sindikatkartu <skreload.server@gmail.com>
2026-08-24 01:25:35 +0700

    v1.17.0: rilis v1.17.0

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *   - Berkas aplikasi (HTML/CSS/JS) : cache-first  -> aplikasi selalu membuka instan
  *   - Panggilan API                 : network-only -> tidak pernah di-cache, agar data tak basi
  */
-const CACHE = 'possk-v1.16.0';
+const CACHE = 'possk-v1.17.0';
 const BERKAS = [
   './', './index.html',
   './css/app.css',
